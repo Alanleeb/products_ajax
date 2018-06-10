6f88587a9c1c1d70d0ead6f34b8fe7dc4fd5a37c
Alan Bischoff <alanlbischoff@gmail.com>
2018-06-10 14:25:21 -0600

    post function works

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,53 +1,50 @@
-function toggleForm() {
-    showForm = !showForm;
-    $('#product-form').remove();
-    $('#product').text('Products');
-    $('#products').remove();
-    $('#products-list').toggle()
-  
-    if (showForm) {
-      $.ajax({
-        url: '/product_form',
-        method: 'GET',
-        data: { id: editingForm }
-      }).done( function(html) {
-        $('#toggle').after(html)
-      });
-    }
-  }
+var del = '<button id=delete-item> Delete</button>'
+var url = ' http://json-server.devpointlabs.com/api/v1/products/'
 
 $(document).ready( function() {
       $.ajax({
-        url: ' http://json-server.devpointlabs.com/api/v1/products/',
+        url: url,
         method: 'GET',
         dataType: 'JSON'
       }).done( function(products) {
         var list = $('#products');
-        list.empty();
         products.forEach( function(char) {
           var li = '<li data-product-id="' + char.id + '">' + char.name + '</li>'
           list.append(li)
         });
       });
-  });
-  (document).on('submit', '#product-form form', function(e) {
-    e.preventDefault(); //stops from going to server
-    var data = $(this).serializeArray();
-    var url = '/products';
-    var method = 'POST'
-    if (editingGame) {
-      url = url + '/';
-      method = 'PUT'
-    }
-    $.ajax({
-      url: url,
-      type: method,
-      dataType: 'JSON',
-      data: data
-    }).done( function(product) {
-      toggleForm()
-      getGame(product.id)
-    }).fail( function(err) {
-      alert(err.responseJSON.errors)
-    });
-  });
+      $('#newitem').on('click', function() {
+       let name = $('#name').val()
+       let price = $('#price').val()
+       let description = $('#description').val()
+       var item = {product: {name: name, price: price, description: description}}
+      $.ajax({
+        url: url,
+        method: 'POST',
+        dataType: 'JSON',
+        data: item
+      }).done( function(item){
+        var list = $('#products');
+        var li = '<li data-product-id="' + item.id + '">' + item.name + ' </li>'
+        list.append(li)
+        $('#name').val("")
+        $('#price').val("")
+        $('#description').val("")
+      })
+    })
+    $(del).on('click', '#delete-item', function(){
+      var id = $(this).siblings('products').dataset().id 
+      debugger
+     $.ajax({
+       url: url + id,
+       method: 'DELETE'
+      }).done( function(msg) {
+        var row = $("[data-id='" + id + "'")
+        row.parent().remove('li');
+        alert(msg.message)
+      })
+    })
+
+  })
+
+ 
